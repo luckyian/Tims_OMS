bd6cf21703f549b5adf512bb83be3c5920d1aced
Ian Wren <ian.wren81@gmail.com>
2021-07-07 14:48:40 -0700

    Deleted old code and started new with different base code

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Tims_OMS
-Order Management System for Tim's Cascade DSD Vendor
+# Fitness Tracker 
+  !["Unlicense/Other"](https://img.shields.io/badge/license-Unlicense-blue.svg)
+
+  ## **Table of Contents**
+  1. [Description](#Description)
+  2. [Technology](#Technology)
+  3. [Usage](#Usage)
+  4. [License](#License)
+  5. [Contributors](#Contributors)
+  6. [Questions](#Questions)
+  
+  > ## **Description**
+  ### This is a Mongo DB based fitness tracker that logs added workouts and saves them to a database.  There are different fields the user fills out and then submits to the database where it is saved.
+ 
+  > ## **Technology**
+  ### Mongojs, Express, Morgan, Mongoose, HTML, Javascript, CSS, Atlas, Heroku, Nodejs
+ 
+  > ## **Usage**
+  ### ![Fitness Tracker](./assets/Fitness-Tracker.gif)
+
+  > ## **License**
+  ### You have choosen the Unlicense/Other license.  For further information on licenses please visit [Choose a License](https://choosealicense.com/)
+  
+  > ## **Contributors**
+  ### Ian Wren
+ 
+
+
+  > ### *Questions?*
+   
+  #### GitHub: [LuckyIan](https://github.com/LuckyIan) 
+  #### Github Repo: [Fitness Tracker] (https://github.com/LuckyIan/https://github.com/luckyian/Fitness_Tracker)
+   
+
+
+  
+
--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
+const express = require("express");
+const mongojs = require("mongojs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
-var PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(logger("dev"));
 
-// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/chips_controller.js");
 
-app.use(routes);
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/fitness_tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+  app.use(require("./routes/api-routes.js"));
+  app.use(require("./routes/html-routes.js"));
+
+
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+      console.log("App running on port" + PORT);
+  });
