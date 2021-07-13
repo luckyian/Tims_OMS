18979af48883e71d5165eef6657e200ba0581fb2
Ian Wren <ian.wren81@gmail.com>
2021-07-12 19:08:47 -0700

    Refactored to new api calls and changes to rest of code

--- public/order2.js
+++ public/order2.js
@@ -16,12 +16,12 @@ const distanceInput = document.querySelector("#distance");
 const completeButton = document.querySelector("button.complete");
 const addButton = document.querySelector("button.add-another");
 const toast = document.querySelector("#toast");
-const neworder = document.querySelector(".new-order")
+const neworder = document.querySelector(".new-order");
 
 let storeType = null;
 let shouldNavigateAway = false;
 
-async function initExercise() {
+async function initOrder() {
   let order;
 
   if (location.search.split("=")[1] === undefined) {
@@ -34,14 +34,14 @@ async function initExercise() {
 
 }
 
-initExercise();
+initOrder();
 
-function handlestoreTypeChange(event) {
+function handleStoreTypeChange(event) {
   storeType = event.target.value;
 
   if (storeType === ("811", "608", "1477", "122", "1455")) {
     storeForm.classList.remove("d-none");
-    pick-listForm.classList.add("d-none");
+    pick - listForm.classList.add("d-none");
   } else if (storeType === "pick-list") {
     pickListForm.classList.remove("d-none");
     storeForm.classList.add("d-none");
@@ -73,7 +73,7 @@ function validateInputs() {
       isValid = false;
     }
 
-    if (pick-listDurationInput.value.trim() === "") {
+    if (pick - listDurationInput.value.trim() === "") {
       isValid = false;
     }
   } else if (storeType === ("811", "608", "1477", "122", "1455")) {
@@ -88,73 +88,86 @@ function validateInputs() {
     if (distanceInput.value.trim() === "") {
       isValid = false;
     }
+    if (weightInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (setsInput.value.trim() === "") {
+      isValid = false;
+    }
+
+    if (repsInput.value.trim() === "") {
+      isValid = false;
+    }
+  }
+    if (isValid) {
+      completeButton.removeAttribute("disabled");
+      addButton.removeAttribute("disabled");
+    } else {
+      completeButton.setAttribute("disabled", true);
+      addButton.setAttribute("disabled", true);
+    }
   }
 
-  if (isValid) {
-    completeButton.removeAttribute("disabled");
-    addButton.removeAttribute("disabled");
-  } else {
-    completeButton.setAttribute("disabled", true);
-    addButton.setAttribute("disabled", true);
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    let orderData = {};
+
+    if (storeType === ("811", "608", "1477", "122", "1455")) {
+      orderData.type = storeType;
+      orderData.name = orderNameInput.value.trim();
+      orderData.distance = Number(distanceInput.value.trim());
+      orderData.duration = Number(durationInput.value.trim());
+      orderData.weight = Number(weightInput.value.trim());
+      orderData.sets = Number(setsInput.value.trim());
+      orderData.reps = Number(repsInput.value.trim());
+    } else if (storeType === "pick-list") {
+      orderData.type = "pick-list";
+      orderData.name = nameInput.value.trim();
+      orderData.weight = Number(weightInput.value.trim());
+      orderData.sets = Number(setsInput.value.trim());
+      orderData.reps = Number(repsInput.value.trim());
+      orderData.duration = Number(pick - listDurationInput.value.trim());
+    }
+
+    await API.addOrder(orderData);
+    clearInputs();
+    toast.classList.add("success");
   }
-}
 
-async function handleFormSubmit(event) {
-  event.preventDefault();
-
-  let orderData = {};
-
-  if (storeType === ("811", "608","1477","122","1455")) {
-    orderData.type = storeType;
-    orderData.name = orderNameInput.value.trim();
-    orderData.distance = Number(distanceInput.value.trim());
-    orderData.duration = Number(durationInput.value.trim());
-  } else if (storeType === "pick-list") {
-    orderData.type = "pick-list";
-    orderData.name = nameInput.value.trim();
-    orderData.weight = Number(weightInput.value.trim());
-    orderData.sets = Number(setsInput.value.trim());
-    orderData.reps = Number(repsInput.value.trim());
-    orderData.duration = Number(pick-listDurationInput.value.trim());
+  function handleToastAnimationEnd() {
+    toast.removeAttribute("class");
+    if (shouldNavigateAway) {
+      location.href = "/";
+    }
   }
 
-  await API.addOrder(orderData);
-  clearInputs();
-  toast.classList.add("success");
-}
-
-function handleToastAnimationEnd() {
-  toast.removeAttribute("class");
-  if (shouldNavigateAway) {
-    location.href = "/";
+  function clearInputs() {
+    orderNameInput.value = "";
+    nameInput.value = "";
+    setsInput.value = "";
+    distanceInput.value = "";
+    durationInput.value = "";
+    repsInput.value = "";
+    pickListDurationInput.value = "";
+    weightInput.value = "";
   }
-}
 
-function clearInputs() {
-  orderNameInput.value = "";
-  nameInput.value = "";
-  setsInput.value = "";
-  distanceInput.value = "";
-  durationInput.value = "";
-  repsInput.value = "";
-  pickListDurationInput.value = "";
-  weightInput.value = "";
-}
+  if (storeTypeSelect) {
+    storeTypeSelect.addEventListener("change", handlestoreTypeChange);
+  }
+  if (completeButton) {
+    completeButton.addEventListener("click", function (event) {
+      shouldNavigateAway = true;
+      handleFormSubmit(event);
+    });
+  }
+  if (addButton) {
+    addButton.addEventListener("click", handleFormSubmit);
+  }
+  toast.addEventListener("animationend", handleToastAnimationEnd);
 
-if (storeTypeSelect) {
-  storeTypeSelect.addEventListener("change", handlestoreTypeChange);
-}
-if (completeButton) {
-  completeButton.addEventListener("click", function (event) {
-    shouldNavigateAway = true;
-    handleFormSubmit(event);
-  });
-}
-if (addButton) {
-  addButton.addEventListener("click", handleFormSubmit);
-}
-toast.addEventListener("animationend", handleToastAnimationEnd);
-
-document
-  .querySelectorAll("input")
-  .forEach(element => element.addEventListener("input", validateInputs));
+  document
+    .querySelectorAll("input")
+    .forEach(element => element.addEventListener("input", validateInputs));
